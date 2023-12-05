1f6d73c4b00537b1d4d5735410b42cc572e0e7d8
diem2271 <diem2271@gmail.com>
2023-12-05 08:22:31 +0300

    camera

--- src/main/java/Editor/TestGame.java
+++ src/main/java/Editor/TestGame.java
@@ -1,11 +1,10 @@
 package Editor;
 
-import Engine.ILogic;
-import Engine.ObjectLoader;
-import Engine.RenderManager;
-import Engine.WindowManager;
+import Engine.*;
+import Engine.entity.Entity;
 import Engine.entity.Model;
 import Engine.entity.Texture;
+import org.joml.Vector3f;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL11;
 
@@ -13,64 +12,75 @@ import org.lwjgl.opengl.GL11;
 
 public class TestGame implements ILogic {
 
-    private int direction = 0;
-    private float colour = 0.0f;
-
+    private static final float CAMERA_MOVE_SPEED = 0.05f;
     private final RenderManager renderer;
     private final ObjectLoader loader;
     private final WindowManager window;
-    private Model model;
+    private Entity entity;
+    private Camera camera;
+
+    Vector3f cameraInc;
 
     public TestGame(){
         renderer = new RenderManager();
         window = Main.getWindow();
         loader = new ObjectLoader();
+        camera = new Camera();
+        cameraInc = new Vector3f(0,0,0);
     }
 
 
     @Override
     public void init() throws Exception {
         renderer.init();
-        float[] vertices = {
-                -0.5f,  0.5f, 0f,
-                -0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0.5f,  0.5f, 0f,
+
+        float[] vertices = new float[] {
+                -0.5f,  0.5f, -0.5f,
+                -0.5f, -0.5f, -0.5f,
+                0.5f, -0.5f, -0.5f,
+                0.5f,  0.5f, -0.5f
+
         };
-        int[] indices = {
-                0,1,3,
-                3,1,2
+        float[] textCoords = new float[]{
+                0f,1f,
+                0f,0f,
+                1f,0f,
+                1f,1f
+        };
+        int[] indices = new int[]{
+                0, 1, 2,
+                2, 3, 0
         };
 
-        float[] textureCoords = {
-                0,0,
-                0,1,
-                1,1,
-                1,0
-        };
-
-        model = loader.loadModel(vertices,textureCoords,indices);
-        model.setTexture(new Texture(loader.loadTexture("textures/grassblock.jpg")));
+        Model model = loader.loadModel(vertices,textCoords,indices);
+        model.setTexture(new Texture(loader.loadTexture("textures/grassblock.png")));
+        entity = new Entity(model, new Vector3f(0,0,5), new Vector3f(0,0,0),1);
     }
 
     @Override
     public void input() {
-        if(window.isKeyPressed(GLFW.GLFW_KEY_UP)){
-            direction=1;
-        } else if (window.isKeyPressed(GLFW.GLFW_KEY_DOWN)) {
-            direction=-1;
-        }else{
-            direction = 0;
-        }
+        cameraInc.set(0,0,0);
+        if(window.isKeyPressed(GLFW.GLFW_KEY_W))
+            cameraInc.z = -1;
+        if(window.isKeyPressed(GLFW.GLFW_KEY_S))
+            cameraInc.z = 1;
+
+        if(window.isKeyPressed(GLFW.GLFW_KEY_A))
+            cameraInc.x = -1;
+        if(window.isKeyPressed(GLFW.GLFW_KEY_D))
+            cameraInc.x = 1;
+
+        if(window.isKeyPressed(GLFW.GLFW_KEY_Z))
+            cameraInc.y = -1;
+        if(window.isKeyPressed(GLFW.GLFW_KEY_X))
+            cameraInc.y = 1;
     }
 
     @Override
     public void update() {
-        colour+= direction * 0.01f;
-        if(colour > 1)
-            colour = 1.0f;
-        else if(colour<=0)
-            colour = 0.0f;
+        camera.movePosition(cameraInc.x * CAMERA_MOVE_SPEED,cameraInc.y * CAMERA_MOVE_SPEED,cameraInc.z * CAMERA_MOVE_SPEED);
+
+//        entity.incRotation(0.0f,0.5f,0.1f);
     }
 
     @Override
@@ -80,8 +90,8 @@ public class TestGame implements ILogic {
             window.setResize(true);
         }
 
-        window.setClearColour(colour,colour,colour,0.0f);
-        renderer.render(model);
+        window.setClearColour(1.0f,0.0f,0.0f,0.0f);
+        renderer.render(entity,camera);
     }
 
     @Override
